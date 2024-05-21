9752bbd778636e084ef5f610fff9da04e9615359
natasha <5155395+murfel@users.noreply.github.com>
2024-05-21 11:47:40 +0100

    Properties vs Accessors vs Strings

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,23 @@
 package org.example
 
 import org.jetbrains.kotlinx.dataframe.DataFrame
+import org.jetbrains.kotlinx.dataframe.api.column
 import org.jetbrains.kotlinx.dataframe.api.print
 import org.jetbrains.kotlinx.dataframe.io.read
 
 // https://kotlin.github.io/dataframe/gettingstartedgradle.html
 
 fun main() {
-    var df = DataFrame.read("movies.csv")
-    df.print()
+    val df = DataFrame.read("movies.csv")
+    println(df.columnNames())  // [movieId, title, genres]
+
+    // Properties - doesn't work "Unresolved reference: title"
+//    df.title
+
+    // Accessors - OK
+    val title by column<String>()
+    df[title].print()
+
+    // Strings - OK
+    df["title"].print()
 }
